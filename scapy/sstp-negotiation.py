6698d8e3754e2cda13f8696a79161da53a54b619
takeshixx <takeshix@adversec.com>
2013-11-14 14:32:57 +0100

    author added

--- scapy/sstp-negotiation.py
+++ scapy/sstp-negotiation.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2
+# author: takeshix@adversec.com
 import ssl, socket
 from sstp import *
 
--- scapy/sstp.py
+++ scapy/sstp.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2
+# author: takeshix@adversec.com
 from scapy.packet import *
 from scapy.fields import *
 
